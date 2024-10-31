21883998e9b82009bb34fb816185c4131e89fec3
Hale <dph7913@utulsa.edu>
2024-10-31 12:15:54 -0500

    halfway done

--- Problem 3/stepCounter.py
+++ Problem 3/stepCounter.py
@@ -1,10 +1,13 @@
 def feet_to_steps(user_feet):
-   #write your code here
-   return
+   steps = int(user_feet/2.5)
+   return steps
 
 if __name__ == '__main__':
     #take input feet steps here
     #store it into the function
+    user_feet = float(input())
+    print(f"{feet_to_steps(user_feet)}")
+
     
     #print your steps here
     feet_to_steps(5280)
--- problem 4/intToBin.py
+++ problem 4/intToBin.py
@@ -1,23 +1,27 @@
 def int_to_reverse_binary(num1):
     binary_val = ''
 #write your while loop here
-    #while num1 > 0:
-        #write your code
+    while num1 > 0:
+        binary_val += str(num1 % 2)
+        num1 = num1 // 2
+    
+    return binary_val
 
-    return binary_val;
-
-
-def string_reverse(input_string): 
+def string_reverse(binary_val): 
     reverse_input = ''
     
+    for digit in range(len(binary_val) -1, -1, -1):
+        reverse_input += binary_val[digit]
    #write your for loop here
     
     return reverse_input
 
 if __name__ == '__main__':
-    user_input = int(input())
-    
-    binary_string = str(int_to_reverse_binary(user_input))
+    num1 = int(input())
+    binary_string = str(int_to_reverse_binary(num1))
     binary_string = str(string_reverse(binary_string))
     
-    print(binary_string)
+    if binary_string == '':
+        print("0")
+    else:
+        print(binary_string)
